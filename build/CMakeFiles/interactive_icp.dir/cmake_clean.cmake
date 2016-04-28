6fc7dc3e4e8d3a96f980dfbb298b814034275e73
Michał Bednarek <michalbednarek.e@gmail.com>
2016-04-28 13:48:21 +0200

    Added files via upload

---
+++ build/CMakeFiles/interactive_icp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/interactive_icp.dir/main.cpp.o"
+  "interactive_icp.pdb"
+  "interactive_icp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/interactive_icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
